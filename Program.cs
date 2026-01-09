63ab369ea042fcd39382d7c4a27408b794eae982
unknown <abdulganiyuabdulaziz646@gmail.com>
2026-01-09 11:58:32 +0000

    input and output

--- Program.cs
+++ Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Prompt for first name
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
+        // Prompt for last name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
+        // Display the result
+        Console.WriteLine();
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
